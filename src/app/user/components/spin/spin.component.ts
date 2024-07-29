1020ad463940e8e89bff46d91df7d9f91f388356
Smruti Ranjan <Smrutiranjan93>
2024-07-29 22:28:47 +0530

    updated spin code

--- src/app/user/components/spin/spin.component.ts
+++ src/app/user/components/spin/spin.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PrizeModalComponent } from "../prize-modal/prize-modal.component";
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import Chart, { ChartItem } from 'chart.js/auto';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-spin',
@@ -10,35 +12,104 @@ import { PrizeModalComponent } from "../prize-modal/prize-modal.component";
   templateUrl: './spin.component.html',
   styleUrl: './spin.component.scss',
 })
-export class SpinComponent {
-  @ViewChild('container') container!: ElementRef<HTMLDivElement>;
-  prizes: number[] = [10, 20000, 50, 5000, 100, 250, 100000, 500, 1000, 2500, 10000, 50000];
-  activeBtn: boolean = false;
-  deg: number = 0;
-  prize: number | null = null;
+export class SpinComponent implements AfterViewInit {
+  @ViewChild('wheel', { static: true })
+  wheelRef!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('spinBtn', { static: true })
+  spinBtnRef!: ElementRef<HTMLButtonElement>;
+  @ViewChild('finalValue', { static: true })
+  finalValueRef!: ElementRef<HTMLDivElement>;
+  totalSpinsAvailable = 10;
+  completedSpins = 5;
+  awardedMoneyToday = '$100';
+  totalWinnings = '$500';
+
+ 
+  private rotationValues = [
+    { minDegree: 0, maxDegree: 30, value: 2 },
+    { minDegree: 31, maxDegree: 90, value: 1 },
+    { minDegree: 91, maxDegree: 150, value: 6 },
+    { minDegree: 151, maxDegree: 210, value: 5 },
+    { minDegree: 211, maxDegree: 270, value: 4 },
+    { minDegree: 271, maxDegree: 330, value: 3 },
+    { minDegree: 331, maxDegree: 360, value: 2 }
+  ];
+
+  private data = [16, 16, 16, 16, 16, 16];
+  private pieColors = [
+    "#8b35bc", "#b163da", "#8b35bc", "#b163da",
+    "#8b35bc", "#b163da"
+  ];
+
+  private myChart: any;
+  private count = 0;
+  private resultValue = 101;
+
+  ngAfterViewInit(): void {
+    this.initializeChart();
+  }
+
+  initializeChart() {
+    const ctx = this.wheelRef.nativeElement.getContext('2d') as ChartItem | null;
+    if (ctx) {
+      this.myChart = new Chart(ctx, {
+        plugins: [ChartDataLabels],
+        type: 'pie',
+        data: {
+          labels: [1, 2, 3, 4, 5, 6],
+          datasets: [{
+            backgroundColor: this.pieColors,
+            data: this.data,
+          }],
+        },
+        options: {
+          responsive: true,
+          animation: { duration: 0 },
+          plugins: {
+            tooltip: {
+              enabled: false
+            },
+            legend: { display: false },
+            datalabels: {
+              color: "#ffffff",
+              formatter: (_: any, context: any) => context.chart.data.labels[context.dataIndex],
+              font: { size: 24 },
+            },
+          },
+        },
+      });
+    }
+  }
+
+  valueGenerator(angleValue: number) {
+    for (let i of this.rotationValues) {
+      if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+        this.finalValueRef.nativeElement.innerHTML = `<p>You Won Rs: ${i.value}</p>`;
+        this.spinBtnRef.nativeElement.disabled = false;
+        break;
+      }
+    }
+  }
 
   spin() {
-    this.activeBtn = true;
-    setTimeout(() => (this.activeBtn = false), 5100);
+    this.spinBtnRef.nativeElement.disabled = true;
+    this.finalValueRef.nativeElement.innerHTML = `<p>Good Luck!</p>`;
+    let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
 
-    let spins = Math.floor(Math.random() * 7) + 9; // perform between 9 and 15 spins
-    console.log('spins: ' + spins);
+    let rotationInterval = window.setInterval(() => {
+      this.myChart.options.rotation = this.myChart.options.rotation + this.resultValue;
+      this.myChart.update();
 
-    let wheelAngle = Math.floor(Math.random() * 12) * 30; // set wheel angle rotation
-    console.log('wheelAngle: ' + wheelAngle);
-
-    let sectorAngle = Math.floor(Math.random() * 14) + 1; // set sector angle rotation
-    sectorAngle *= Math.floor(Math.random() * 2) === 1 ? 1 : -1; // between -14deg and +14deg (28deg range of 30deg sector)
-    console.log('sectorAngle: ' + sectorAngle);
-
-    this.deg += 360 * spins + wheelAngle + sectorAngle;
-    (this.container.nativeElement.querySelector('.inner') as HTMLElement).style.transform = `rotate(${this.deg}deg)`;
-
-    setTimeout(() => (this.deg -= sectorAngle), 100); // reset sector angle rotation to avoid angle > +-44deg on next rotation
-
-    let index = Math.floor((this.deg - sectorAngle) / 30) % 12; // get the prize
-    console.log('prize index:' + index);
-    this.prize = this.prizes[index];
-    console.log('you will win: ' + this.prize);
+      if (this.myChart.options.rotation >= 360) {
+        this.count += 1;
+        this.resultValue -= 5;
+        this.myChart.options.rotation = 0;
+      } else if (this.count > 15 && this.myChart.options.rotation === randomDegree) {
+        this.valueGenerator(randomDegree);
+        clearInterval(rotationInterval);
+        this.count = 0;
+        this.resultValue = 101;
+      }
+    }, 10);
   }
 }
