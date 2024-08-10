10f521b1dccfe51104b158adfb494cfca4dfaeda
Smruti Ranjan <Smrutiranjan93>
2024-08-10 08:00:23 +0530

    finance price updated

--- src/app/user/components/home/home.component.ts
+++ src/app/user/components/home/home.component.ts
@@ -6,33 +6,56 @@ import { Router, RouterModule, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,FormsModule,ReactiveFormsModule,RouterModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   selectedPlan: string | null = null;
-constructor(private route :Router){}
-  planDetails: { [key: string]: Array<{ feature: string, description: string }> } = {
-    freelance: [
-      { feature: 'Space', description: '1 GB of space' },
-      { feature: 'Support', description: 'Support at $25/hour' },
-      { feature: 'Cloud Access', description: 'Limited cloud access' }
+  constructor(private route: Router) {}
+  planDetails: {
+    [key: string]: Array<{ feature: string; description: string }>;
+  } = {
+    basic: [
+      { feature: 'Cameras', description: 'Includes 2 Cameras' },
+      { feature: 'Cashback', description: 'Up to ₹25,000 Cashback' },
+      { feature: 'Installation', description: 'Standard Installation' },
     ],
-    business: [
-      { feature: 'Space', description: '5 GB of space' },
-      { feature: 'Support', description: 'Support at $5/hour' },
-      { feature: 'Cloud Access', description: 'Full cloud access' }
+    standard: [
+      { feature: 'Cameras', description: 'Includes 4 Cameras' },
+      { feature: 'Cashback', description: 'Up to ₹50,000 Cashback' },
+      { feature: 'Installation', description: 'Premium Installation' },
     ],
-    enterprise: [
-      { feature: 'Space', description: '10 GB of space' },
-      { feature: 'Support', description: 'Support at $5/hour' },
-      { feature: 'Cloud Access', description: 'Full cloud access' }
+    premium: [
+      { feature: 'Cameras', description: 'Includes 6 Cameras' },
+      { feature: 'Cashback', description: 'Up to ₹100,000 Cashback' },
+      { feature: 'Installation', description: 'Advanced Installation' },
+    ],
+    ultimate: [
+      { feature: 'Cameras', description: 'Includes 8 Cameras' },
+      { feature: 'Cashback', description: 'Up to ₹20,000 Cashback' },
+      { feature: 'Installation', description: 'Complete Installation' },
+    ],
+    ultraPremium:[
+      { feature: 'Cameras', description: 'Includes 15 Cameras' },
+      { feature: 'Cashback', description: 'Up to ₹2,50,000 Cashback' },
+      { feature: 'Installation', description: 'Complete Installation' },
+    ],
+    superSonic:[
+      { feature: 'Cameras', description: 'Includes 35 Cameras' },
+      { feature: 'Cashback', description: 'Up to ₹5,00,000 Cashback' },
+      { feature: 'Installation', description: 'Complete Installation' },
     ]
   };
 
   selectPlan(plan: string): void {
     this.selectedPlan = plan;
-// this.route.navigateByUrl('user/payment')
+    // this.route.navigateByUrl('user/payment')
   }
 }
