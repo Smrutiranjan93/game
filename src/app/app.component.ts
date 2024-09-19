390c081301247211f09efaba3aae9cb4ce7f75c4
Smruti Ranjan <Smrutiranjan93>
2024-09-19 09:05:52 +0530

    travel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,16 @@
-import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './common/header/header.component';
-import { FooterComponent } from './common/footer/footer.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,CommonModule,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
-
-  constructor(private router: Router) {}
-  shouldHideHeaderFooter(): boolean {
-    const currentRoute = this.router.url;
-    return currentRoute === '/login' || currentRoute === '/register'||  currentRoute.startsWith('/user');;
-  }
-
- 
+  title = 'travel-booking';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,8 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideToastr } from 'ngx-toastr';
-import { provideAnimations } from '@angular/platform-browser/animations';
-// export const appConfig: ApplicationConfig = {
-//   providers: [provideRouter(routes),importProvidersFrom(HttpClientModule)]
-// };
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideHttpClient(withFetch()), provideClientHydration(),provideToastr({
-    timeOut: 4000,
-    positionClass: 'toast-top-right',
-    preventDuplicates: true,
-  }), 
-    provideAnimations(),]
+  providers: [provideRouter(routes)]
 };
